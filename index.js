46802f80908ee29c00100910563081d24a4988da
MouhannadAlKhodor <moesidaya@gmail.com>
2021-04-02 21:29:02 +0300

    step 4

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import url from 'url'
 let app = express();
 let port = process.env.port || 3000;
 let router = express.Router();
 app.use("/", router);
+
 
 app.listen(port, () => {
 	console.log(`Node.js application running on port : ${port}`);
@@ -21,5 +23,31 @@ var h = d.getHours();
 var s = d.getSeconds();
 router.get("/time", (req, res) => {
 	res.send(`{status:200, message:${h}:${s}}`);
+});
+
+router.get('/hello/:id', function(req, res) {
+    var id = req.params.id
+    res.send(`{status:200, message:"Hello, ${id}"}`)
+});
+
+app.get('/search',(req,res) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined') {
+        const response = {
+            status:200, message:"ok", data: search
+        };
+
+        res.send(response);
+    }
+    else {
+        const response = {
+            status:500, error:true, message: "you have to provide a search"
+        };
+
+
+        res.status(500);
+        res.send(response);
+    }
 });
 
